d937640cd71f5106d891f7966b7f1907359de61d
Jennifer Davila <pazjenni1331@gmail.com>
2021-07-05 12:52:06 -0500

    Updated template format

---
+++ helloworld.md
@@ -0,0 +1,46 @@
+# **Hello World**
+[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)
+
+## **Table of Contents**
+1. [Description](#description)
+2. [Installation](#installation)
+3. [Usage](#usage)
+4. [Collaborators](#collaborators)
+5. [Contribution Guidelines](#contribution-guidelines)
+6. [Tests](#tests)
+7. [Technologies](#technologies)
+8. [Questions](#questions)
+9. [Contact info](#contact-info)
+10. [License](#license)
+
+## **Description**
+This application allows the user to create a professional readme.
+
+## **Installation**
+In order to install this application, the user must git clone...
+
+## **Usage**
+For the user to navigate through the application, they must first...
+
+## **Collaborators**
+*Jennifer Davila
+
+### *Contribution guidelines*
+In order to collaborate, you must...
+
+## **Tests**
+To test the application...
+
+## **Technologies**
+Javascript and node.js
+
+## **Questions**
+**FAQs**
+How do you create a readme with this application?  You must answer each prompt.
+
+*Questions? Feel free to contact me.*
+* Via [Gtihub](https://github.com/pazjenni04)
+* Via [Email](pazjenni04@yahoo.com)
+
+# License
+[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)
--- script.js
+++ script.js
@@ -65,20 +65,12 @@ inquirer
         message: "Please choose one of the following licensing for your application.",
         name: "license",
         choices: [
-            "The MIT License",
-            "Mozilla Public License 2.0",
-            "Zlib/libpng License",
-            "IBM Publice License Version 1.0",
-            "Apache 2.0 License"
+            "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+            "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)",
+            "[![License: Zlib](https://img.shields.io/badge/License-Zlib-lightgrey.svg)](https://opensource.org/licenses/Zlib)",
+            "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)",
         ],
         validate: (value) =>{if (value){return true} else{return 'Please choose a licensing to continue.'}},
-    },
-    {
-        type: "input",
-        message: "What are some frequently asked questions that could assist the user with your application?",
-        name: "questions",
-        validate: (value) =>{if (value){return true} else{return 'Please provide a list of FAQs to continue.'}},
-        
     },
     {
         type: "input",
@@ -112,10 +104,10 @@ inquirer
     }) => {
 
 const buildreadme = 
-`# ${Title}
-[![License: ${license}]]
+`# **${Title}**
+${license}
 
-## Table of Contents
+## **Table of Contents**
 1. [Description](#description)
 2. [Installation](#installation)
 3. [Usage](#usage)
@@ -127,34 +119,32 @@ const buildreadme =
 9. [Contact info](#contact-info)
 10. [License](#license)
 
-## Description
+## **Description**
 ${description}
 
-## Installation
+## **Installation**
 ${installation}
 
-## Usage
+## **Usage**
 ${usage}
 
-## Collaborators
-${collaborators}
+## **Collaborators**
+* ${collaborators}
 
-### Contribution guidelines
+### *Contribution guidelines*
 ${guidelines}
 
-## Tests
+## **Tests**
 ${test}
 
-## Technologies
-${technologies}
+## **Technologies**
+* ${technologies}
 
-## Questions
-${questions}
+## **Questions**
 
-### Contact info
-Please feel free to contact me via ${communication}.
-* [Gtihub]${github}
-* [Email]${email}
+*Questions? Feel free to contact me.*
+* Via [Gtihub](${github})
+* Via [Email](${email})
 
 # License
 ${license}`
